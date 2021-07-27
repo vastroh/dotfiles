fa8118efd23cad3123e9c967df4292d91dad74b3
Владимир Штро <shtro@nii-ikt.ru>
2021-07-27 15:55:57 +0700

    Добалвен скрипт для переворачивания раскладки текста

---
+++ .i3/i3scripts/flip_text.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Этот скрипт позволяет менять раскладку выделенного текста, т.е. выделяется текст, нажимаются соответствующие клавиши происходит смена раскладки и замена символов выделенного текста…
+xdotool key Ctrl+Insert
+sleep 0.1s
+xsel | sed "y/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]{};':\",.\/<>?@#\$^&\`~фисвуапршолдьтщзйкыегмцчняФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯхъХЪжэЖЭбюБЮ№ёЁ/фисвуапршолдьтщзйкыегмцчняФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯхъХЪжэЖЭбю.БЮ,\"№;:?ёЁabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]{};':\",.<>#\`~/" | xsel -b -i
+sleep 0.1s
+xdotool key Shift+Insert
+sleep 0.1s
+# xdotool key Caps_Lock
