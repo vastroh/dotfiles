1c7b98dedfff13524cdbabbbf05c7ae211785d8a
Владимир Штро <shtro@nii-ikt.ru>
2023-05-03 12:52:07 +0700

    WIP

--- update_config.sh
+++ update_config.sh
@@ -1,4 +1,3 @@
 cp -r .config/polybar/* ~/.config/polybar/
 cp .config/i3wsr/config.toml ~/.config/i3wsr/
-cp .i3/config ~/.config/i3/
-cp -r .i3/scripts/* ~/.config/i3/scripts/
+cp -r .i3/* ~/.config/i3/
